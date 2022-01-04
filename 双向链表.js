b4525071e29a9e4ee0c1d9572903a1d188499afe
彭格列 <1801688332@qq.com>
2022-01-04 17:40:52 +0800

    feat: 双向链表 append

--- 双向链表.js
+++ 双向链表.js
@@ -2,3 +2,36 @@
 
 
 // 双向链表
+function DoublyLineList() {
+  
+
+  // 定义一个内部类
+  function Node(data) {
+    this.data = data;
+    this.next = null; // 向后指向
+    this.prevs = null; // 向前指向
+  }
+
+  // 属性
+  this.head = null; // 头
+  this.tail = null; // 尾
+  this.length = 0;
+
+
+  // append 依次插入某一些数据
+  DoublyLineList.prototype.append = function(data) {
+    let newNode = new Node(data);
+    // 判断是否是第一次插入
+    if (this.length == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // 在有节点的情况下进行判断
+      newNode.prevs = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length += 1;
+  }
+}
