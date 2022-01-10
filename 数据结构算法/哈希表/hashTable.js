e4e41a40d5ed94d46d44935b81adba943471d3f4
彭格列 <1801688332@qq.com>
2022-01-10 13:07:18 +0800

    feat: 哈希插入/修改

--- 数据结构算法/哈希表/hashTable.js
+++ 数据结构算法/哈希表/hashTable.js
@@ -9,12 +9,62 @@
  * 存在key，修改操作
  */
 
-function hashTable(params) {
+function HashTable() {
   // 属性
   this.storage = []; // 数组中存放相关的元素
   this.count = 0; // 当前已经存在了多少数据
-  this.limit = 0; // 标记数组中一共可以存放多少个元素
+  this.limit = 5; // 初始设置数组的长度
 
-  // 方法
+  // 方法 / 哈希函数
+  HashTable.prototype.hashFn = function(str, size) {
+    let hashCode = 0;
+    
+    // 将字符串转 => 数字 unicode 
+    for (let i = 0; i < str.length; i++) {
+      hashCode = 37 * hashCode + str.charCodeAt(i);
+    }
+  
+    // 取余操作
+    let index = hashCode % size;
+  
+    return index;
+  }
+
+  // 插入/修改
+  /**
+   * 1. 根据key获取索引值
+   *    目的：将数据插入到对应到位置
+   * 2. 根据索引值取出bucket
+   *  a. 如果bucket不存在，创建bucket并且防止在该索引的位置
+   * 3. 判断新增还是修改原来的值
+   *  a. 如果已经有值，那么修改值
+   *  b. 如果没有，执行后续的添加操作
+   */
+
+  HashTable.prototype.put = function(key, value) {
+
+    let currentIndex = this.hashFn(key, this.limit);
+
+    let bucket = this.storage[currentIndex];
+
+    if (bucket == null) {
+      bucket = [];
+      this.storage[currentIndex] = bucket;         
+   }
+
+    for (let i = 0; i < bucket.length; i++) {
+      let tuble = bucket[i];
+      if (tuble[0] == key) {
+        tuble[i] = value;
+        return;
+      }
+    }
+
+
+    bucket.push([key, value]);
+
+    this.count += 1;
+  }
+
 }
 
